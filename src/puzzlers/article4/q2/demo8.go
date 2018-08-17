b58e2e334668c6b515d1d691163174c228e2779d
robert.hao <hypermind@outlook.com>
2018-08-17 19:37:49 +0800

    Fix a bug in demo8.go (thanks Xiaolonghuster)

--- src/puzzlers/article4/q2/demo8.go
+++ src/puzzlers/article4/q2/demo8.go
@@ -8,14 +8,14 @@ import (
 func main() {
 	var name = getTheFlag()
 	flag.Parse()
-	fmt.Printf("Hello, %v!\n", name)
+	fmt.Printf("Hello, %v!\n", *name)
 }
 
-func getTheFlag() string {
-	return *flag.String("name", "everyone", "The greeting object.")
+func getTheFlag() *string {
+	return flag.String("name", "everyone", "The greeting object.")
 }
 
-// 上面函数的实现也可以是这样的。
-//func getTheFlag() int {
-//	return *flag.Int("num", 1, "The number of greeting object.")
+//上面函数的实现也可以是这样的。
+//func getTheFlag() *int {
+//	return flag.Int("num", 1, "The number of greeting object.")
 //}
