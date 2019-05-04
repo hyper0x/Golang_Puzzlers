36ce6849109b7b2bd5e857527e21c20747029a07
robert.hao <hypermind@outlook.com>
2019-05-04 23:30:57 +0800

    Fix a bug in demo7.go

--- src/puzzlers/article4/q1/demo7.go
+++ src/puzzlers/article4/q1/demo7.go
@@ -10,11 +10,14 @@ func main() {
 	flag.StringVar(&name, "name", "everyone", "The greeting object.") // [2]
 
 	// 方式1。
-	//var name = *flag.String("name", "everyone", "The greeting object.")
+	//var name = flag.String("name", "everyone", "The greeting object.")
 
 	// 方式2。
-	//name := *flag.String("name", "everyone", "The greeting object.")
+	//name := flag.String("name", "everyone", "The greeting object.")
 
 	flag.Parse()
-	fmt.Printf("Hello, %v!\n", name)
+	fmt.Printf("Hello, %s!\n", name)
+
+	// 适用于方式1和方式2。
+	//fmt.Printf("Hello, %s!\n", *name)
 }
